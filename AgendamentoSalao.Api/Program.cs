354aa2130f416f60cf833a601338114ad0d1bd23
Gabriel Thales Alves da Silva <gabrielthalles7@gmail.com>
2026-04-13 16:27:51 -0300

    feat: adicionando endpoint get agendamentos e post agendamentos

--- AgendamentoSalao.Api/Program.cs
+++ AgendamentoSalao.Api/Program.cs
@@ -21,5 +21,30 @@ app.UseSwaggerUI();
 // Um endpoint simples de Healthcheck para provar que a API subiu
 app.MapGet("/", () => "API do Salão de Beleza está rodando perfeitamente!");
 
+app.MapPost("/agendamentos", (AgendamentoRequest request, AgendamentoService service) =>
+{
+    try
+    {
+        // Aqui usamos o serviço que você testou com TDD! 
+        // Ele vai validar as regras de negócio antes de salvar.
+        service.Agendar(request);
+        return Results.Created("/agendamentos", request);
+    }
+    catch (ArgumentException ex) // Erro de data no passado
+    {
+        return Results.BadRequest(new { erro = ex.Message });
+    }
+    catch (InvalidOperationException ex) // Erro de conflito de horário
+    {
+        return Results.Conflict(new { erro = ex.Message });
+    }
+});
+
+app.MapGet("/agendamentos", (IAgendamentoRepository repo) =>
+{
+    var agendamentos = repo.ObterTodos();
+    return Results.Ok(agendamentos);
+});
+
 
 app.Run();
